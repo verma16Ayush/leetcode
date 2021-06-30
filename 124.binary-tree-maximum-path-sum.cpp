4c5f91b7c04dafb2cee912dbff7363d6e3bdaecd
Ayush <verma16.ayush@gmail.com>
2021-06-30 23:33:44 +0530

    binary tree max path sum

--- 124.binary-tree-maximum-path-sum.cpp
+++ 124.binary-tree-maximum-path-sum.cpp
@@ -4,7 +4,6 @@
  * [124] Binary Tree Maximum Path Sum
  */
 
-// @lc code=start
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -16,10 +15,22 @@ struct TreeNode {
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
+// @lc code=start
 class Solution {
 public:
+    int ans = INT32_MIN;
+    int help(TreeNode* root)
+    {
+        if(root == nullptr) return 0;
+        int get_left = max(help(root->left), 0);
+        int get_right = max(help(root->right), 0);
+        int cur_max = root->val + get_left + get_right;
+        ans = max(cur_max, ans);
+        return root->val + max(get_left, get_right);
+    }
     int maxPathSum(TreeNode* root) {
-        
+        help(root);
+        return ans;
     }
 };
 // @lc code=end
